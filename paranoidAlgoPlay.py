bbf4f9f70fbc0d5dc12f620305b486a585353660
Josh Holder <jgh8@rice.edu>
2022-01-10 17:28:10 -0500

    adding replay with algo and paranoid algo play

---
+++ paranoidAlgoPlay.py
@@ -0,0 +1,167 @@
+import sys 
+import math
+import os
+from weighting import findWeights
+from core_update import getScreenString
+from core_update import HORIZONTAL
+import core_update as core
+import copy
+import time
+from collections import deque
+import pickle
+import heapq
+
+def playUntilEnd(game_board):
+
+	while 1:
+		moveOptions = game_board.getMoveOptions()
+
+		if len(moveOptions) == 0:
+			break
+
+		best_split_ind = max(range(len(game_board.weights)), key=game_board.weights.__getitem__)
+
+		core.makeMove(game_board,best_split_ind)
+
+	return len(game_board.splitRecord)
+
+def getBoardFromSequence(sequence):
+	gb = core.Board()
+	for split in sequence:
+		core.makeMove(gb,split,calcWeights=False)
+	return gb
+
+if __name__ == "__main__":
+
+	log_name = "sequences/logParanoid1021.txt"
+
+	continuation = True
+	piggyback = False
+
+	weight_diff_tol = 0.25
+
+	#If you want to pick up search from where you left off
+	continuation_name = "sequences/paranoid1021_continuation.pkl"
+	if continuation:
+		with open(continuation_name,'rb') as f:
+			cont_data = pickle.load(f)
+			big_cluster_boards = cont_data[0]
+			big_cluster_weights = cont_data[1]
+			best_sequence_len = cont_data[2]
+			exploring_from = cont_data[3] #splits
+			restart_splits = cont_data[4]
+
+			game_board = big_cluster_boards.pop()
+			game_board.weights = big_cluster_weights.pop()
+	else:
+		big_cluster_boards = []
+		big_cluster_weights = []
+		best_sequence_len = 0
+		exploring_from = 2000 #splits
+		restart_splits = 0
+		#clear log
+		log_file = open(log_name,'w')
+		log_file.close()
+
+		game_board = core.Board()
+
+	#If you want to explore from an already existing sequence:
+	if piggyback:
+		sequenceFile = "sequences/combo1021.txt"
+		try:
+			f = open(sequenceFile, "r")
+		except:
+			print("Couldn't open file",sequenceFile,", does it exist?")
+			sys.exit(1)
+
+		line=f.readline()
+		f.close()
+		if line[0] == "[": line = line[1:]
+		if line[-1] == "]": line = line[:-1]
+		piggyback_sequence=(line.rstrip('\n').split(', '))
+		piggyback_sequence=[int(ii) for ii in piggyback_sequence]
+
+	game_num = 0
+
+	while 1:
+		moveOptions = game_board.getMoveOptions()
+
+		if len(moveOptions) == 0:
+			game_num += 1
+
+
+			#Every 50 games, update the saved version of your continuation list
+			if game_num % 50 == 0:
+				with open(continuation_name,'wb') as cf:
+					pickle.dump([big_cluster_boards,big_cluster_weights,best_sequence_len,exploring_from,restart_splits],cf)
+
+			with open(log_name,'a') as lf:
+				lf.write("\tLost at {} splits (base state = {})\n".format(len(game_board.splitRecord),exploring_from))
+				#If this is a new record, record that
+				if len(game_board.splitRecord) > best_sequence_len:
+					print("---------------New Record! {} Splits ({} games)---------------".format(len(game_board.splitRecord),game_num))
+					best_sequence_len = len(game_board.splitRecord)
+					lf.write("~~New Record! {} Splits (after {} games)~~\n".format(best_sequence_len,game_num))
+					lf.write("{}\n".format(game_board.splitRecord))
+
+				#pop off last gameboard which caused a big cluster, restart from here
+				if len(big_cluster_boards) > 0:
+					game_board = big_cluster_boards.pop()
+					game_board.weights = big_cluster_weights.pop()
+					lf.write("\t\tRestarting at {} splits\n".format(len(game_board.splitRecord)))
+					restart_splits = len(game_board.splitRecord)
+				else:
+					lf.write("Exhausted all options - no more big clusters to remove. Highest splits reached was {}".format(len(game_board.splitRecord)))
+
+				#If this is the lowest split board we are expanding, update this:
+				if len(game_board.splitRecord) < exploring_from:
+					exploring_from = len(game_board.splitRecord)
+					print("~~Exploring from new base state - {} splits~~".format(exploring_from))
+					lf.write("Exploring from new base state - {} splits. Board:\n".format(exploring_from))
+					lf.write(getScreenString(game_board))
+					lf.write("Sequence:\n{}\n".format(game_board.splitRecord))
+
+		#remove double weights
+		seen_weights = set()
+		for i,weight in enumerate(list(game_board.weights)):
+			if weight not in seen_weights:
+				seen_weights.add(weight)
+			else:
+				game_board.weights[i] = 0
+
+		high_weight_indices = heapq.nlargest(2,range(len(game_board.weights)), key=game_board.weights.__getitem__)
+		best_split_ind = high_weight_indices[0]
+		#only check weight difference if theres more than one weight (only matters on turn 1)
+		if len(high_weight_indices) > 1:
+			second_best_split_ind = high_weight_indices[1]
+			weight_diff = game_board.weights[high_weight_indices[0]]-game_board.weights[high_weight_indices[1]]
+		else: weight_diff = 100
+
+		if piggyback and game_num == 0: #if piggybacking, select moves from sequence at first
+			best_split_ind = piggyback_sequence[len(game_board.splitRecord)]
+
+		print("Game {}, Split {} - (restart {}, base {})".format(game_num,len(game_board.splitRecord),restart_splits,exploring_from), end='\r')
+
+		old_game_board = copy.deepcopy(game_board)
+
+		core.makeMove(game_board,best_split_ind)
+
+		#After making the move, if the most recent block has points and
+		#is not 1x1, then it made an unecessarily big cluster and we should add it
+		#OR if there was another split option within 5 weight but not identical
+		most_recent_box = game_board.box[-1]
+
+		split_created_big_cluster = ((most_recent_box.points != 0) and (most_recent_box.width != 1 or most_recent_box.height != 1))
+		another_as_good_option = ((weight_diff < 3) and (weight_diff > 1) and len(game_board.splitRecord)>875)
+		if split_created_big_cluster or another_as_good_option:
+			old_weights = list(old_game_board.weights)
+
+			#only add it as an option to take differently if its not the only move
+			weights_without_max = list(old_weights)
+			weights_without_max.pop(best_split_ind) #remove the max weight split
+			if max(weights_without_max) != 0:
+				old_weights[second_best_split_ind] = old_weights[best_split_ind]
+				old_weights[best_split_ind] = 0 #change weight of previously chosen split to zero to prevent it from being chosen again
+
+				big_cluster_boards.append(old_game_board)
+				big_cluster_weights.append(old_weights)
---
+++ replayWithAlgo.py
@@ -0,0 +1,185 @@
+from weighting import findWeights
+import core_update as core
+import sys
+from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtGui import QPainter, QColor, QPen,QBrush
+from PyQt5.QtCore import QRect, Qt
+
+
+##########################################
+class gameBoardDisplay(QWidget):
+##########################################
+    
+    def __init__(self,gameBoard):
+        super().__init__()
+
+        self.minimumTileSize=40
+        self.initUI(gameBoard)
+        self.gameBoard=gameBoard
+        self.nextHighlightedBox=0
+        self.algoHighlightedBoxes=[]
+        
+    def initUI(self,gameBoard):      
+
+        self.setGeometry(300, 300, self.minimumTileSize*gameBoard.width, self.minimumTileSize*gameBoard.height)
+        self.setWindowTitle('Score: {0}\tSplits:{1}'.format(gameBoard.score,len(gameBoard.splitRecord)))
+        self.show()
+        
+    def Update(self,gameBoard):
+        self.gameBoard=gameBoard
+        self.setWindowTitle('Score: {0}\tSplits:{1}'.format(gameBoard.score,len(gameBoard.splitRecord)))
+        self.update()
+
+    def HighlightNextBox(self,nextHighlightedBox):
+        self.nextHighlightedBox=nextHighlightedBox
+
+    def HighlightAlgoBoxes(self,algoHighlightedBoxes):
+        self.algoHighlightedBoxes=algoHighlightedBoxes
+
+    def paintEvent(self, e):
+
+        qp = QPainter()
+        qp.begin(self)
+
+        self.drawBackground(qp)
+
+        for boxindex,box in enumerate(self.gameBoard.box):
+            self.drawBox(qp,box.x,box.y,box.width,box.height,boxindex,box.points)
+            if (boxindex==self.nextHighlightedBox) and (boxindex in self.algoHighlightedBoxes):
+                self.drawBoxHighlight(qp,box.x,box.y,box.width,box.height,Qt.green)
+            elif boxindex==self.nextHighlightedBox:
+                self.drawBoxHighlight(qp,box.x,box.y,box.width,box.height,Qt.red)
+            elif boxindex in self.algoHighlightedBoxes:
+                self.drawBoxHighlight(qp,box.x,box.y,box.width,box.height,Qt.blue)
+
+        qp.end()
+    
+    def drawBackground(self,qp):
+
+        brush = QBrush()
+        brush.setStyle(Qt.SolidPattern)
+        brush.setColor(QColor(230, 230, 230))
+        qp.setBrush(brush)
+
+        rect1=QRect(0, 0, self.minimumTileSize*self.gameBoard.width, self.minimumTileSize*self.gameBoard.height)
+        qp.drawRect(rect1)  
+
+    def drawBox(self,qp,x,y,width,height,index,points):
+        
+        brush = QBrush()
+        brush.setStyle(Qt.SolidPattern)
+        brush.setColor(QColor(150, 150, 150))
+        qp.setBrush(brush)
+
+        pen = QPen(Qt.black, 2, Qt.SolidLine)
+        qp.setPen(pen)
+
+        rect1=QRect(x*self.minimumTileSize, y*self.minimumTileSize, width*self.minimumTileSize, height*self.minimumTileSize)
+
+        if points>0:
+            brush.setStyle(Qt.Dense2Pattern)
+            qp.setBrush(brush)
+            qp.drawRect(rect1)             
+            qp.drawText(rect1, Qt.AlignCenter,"[{0}]\n{1}".format(index,points))    
+        else:
+            qp.drawRect(rect1) 
+            qp.drawText(rect1, Qt.AlignCenter,"[{0}]".format(index)) 
+   
+    def drawBoxHighlight(self,qp,x,y,width,height,color):
+
+        brush = QBrush()
+        brush.setStyle(Qt.NoBrush)
+        qp.setBrush(brush)
+
+        pen = QPen(color, 8, Qt.DashLine)
+        qp.setPen(pen)
+
+        rect1=QRect(x*self.minimumTileSize, y*self.minimumTileSize, width*self.minimumTileSize, height*self.minimumTileSize)
+        qp.drawRect(rect1)
+
+
+##########################################
+def replaySequence(graphicalDisplay,sequence):
+##########################################
+    gameBoard=core.Board(8,16)
+    
+    while 1:
+
+        moveOptions=gameBoard.getMoveOptions()
+
+        if len(moveOptions)==0:
+            return gameBoard.score,gameBoard.splitRecord
+            break
+
+        try: nextMove=sequence[len(gameBoard.splitRecord)]
+        except IndexError: pass
+        
+        print("Move {0} of {1}: Split box #{2}".format(len(gameBoard.splitRecord),len(sequence),nextMove))
+
+        #determine best moves
+        best_indices = [i for i, x in enumerate(gameBoard.weights) if x == max(gameBoard.weights)]
+
+        graphicalDisplay.HighlightNextBox(nextMove)
+        graphicalDisplay.HighlightAlgoBoxes(best_indices)
+        graphicalDisplay.Update(gameBoard)
+        
+        userinput=input("Press any key to continue")
+        if nextMove not in moveOptions:
+            print("------ IMPOSSIBLE MOVE REQUESTED -----")
+            break
+
+        if gameBoard.weights[nextMove] != max(gameBoard.weights):
+            print("~~~Made move not recommended by algorithm.~~~")
+            indices = [i for i, x in enumerate(gameBoard.weights) if x == max(gameBoard.weights)]
+            print("Box(es) recommended:")
+            print(indices)
+            print("Weight of box(es) recommended:")
+            print(max(gameBoard.weights))
+            print("Weight of box chosen:")
+            print(gameBoard.weights[nextMove])
+            # print(gameBoard.weights)
+
+        if userinput == "w":
+            findWeights(gameBoard,weightverbose=1)
+            userinput=input("Press any key to continue")
+
+        core.makeMove(gameBoard,nextMove)       
+
+##########################################
+if __name__ == '__main__':
+##########################################
+    try:
+        sequenceFile=sys.argv[1]
+    except:
+        print("Didn't get the parameters I expected.\n\nExpected usage is replay.py <sequence filename>\n")
+        sys.exit(1)
+
+    print("\n--------Replay SPL-T--------\n")
+
+    try:
+        f = open(sequenceFile, "r")
+    except:
+        print("Couldn't open file",sequenceFile,", does it exist?")
+        sys.exit(1)
+
+    line=f.readline()
+    f.close()
+    if line[0] == "[": line = line[1:]
+    if line[-1] == "]": line = line[:-1]
+    sequence=(line.rstrip('\n').split(', '))
+    sequence=[int(ii) for ii in sequence]
+
+    print("This sequence has a length of",len(sequence))
+    
+
+    app = QApplication(sys.argv)
+
+    gameBoard=core.Board(8,16)    
+
+    graphicalDisplay = gameBoardDisplay(gameBoard)
+
+    score,path=replaySequence(graphicalDisplay,sequence)
+
+    print("\tScore: ",score,"\tLength: ",len(path))
+
+    sys.exit(app.exec_())
